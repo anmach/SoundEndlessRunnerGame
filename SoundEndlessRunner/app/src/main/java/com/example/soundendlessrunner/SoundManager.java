9b78718c8032ea4555684c4ba7326aee172cd211
Anna Mach <anna.mach@euvic.pl>
2021-01-05 21:29:18 +0100

    Whatever

--- SoundEndlessRunner/app/src/main/java/com/example/soundendlessrunner/SoundManager.java
+++ SoundEndlessRunner/app/src/main/java/com/example/soundendlessrunner/SoundManager.java
@@ -5,8 +5,6 @@ import android.media.MediaPlayer;
 import android.media.VolumeShaper;
 
 public class SoundManager {
-    private final long soundTime; //in ms
-
     private VolumeShaper volumeShaper;
 
     private MediaPlayer mediaPlayerObstacle;
@@ -16,8 +14,6 @@ public class SoundManager {
     VolumeShaper.Configuration volumeConfig;
 
     public SoundManager(Context context, long soundTime) {
-        this.soundTime = soundTime;
-
         volumeConfig = new VolumeShaper.Configuration.Builder()
                 .setDuration(soundTime)
                 .setCurve(new float[]{0.f, 1.f}, new float[]{0.f, 1.f})
